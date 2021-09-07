686bbfaf0f62a0ed86ac64821ced923488a02551
unknown <soumitradas407@gmail.com>
2021-09-07 16:22:09 +0530

    add start script

--- index.js
+++ index.js
@@ -101,7 +101,9 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error');
 })
 
-app.listen(3000,()=>{
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
     console.log('Listening to port 3000');
 })
 
